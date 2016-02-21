9b47aee9f1610d7b04070951508e302a17fcb0a7
Rehan Pochkhanawala <rehanp@msn.com>
2016-02-21 22:14:13 +0000

    Iterate thru hash, find total sum of specific value

---
+++ lesson_3/medium2_q1.rb
@@ -0,0 +1,13 @@
+munsters = {
+  "Herman" => { "age" => 32, "gender" => "male" },
+  "Lily" => { "age" => 30, "gender" => "female" },
+  "Grandpa" => { "age" => 402, "gender" => "male" },
+  "Eddie" => { "age" => 10, "gender" => "male" }
+}
+total_age = 0
+munsters.each do |key, value|
+ total_age += value['age'] if value["gender"] == "male"
+end
+
+puts total_age
+    
