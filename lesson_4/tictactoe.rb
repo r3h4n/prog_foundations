499e6ef68bc25eb4083aa1e732ab0290440f0cf8
Rehan Pochkhanawala <rehanp@msn.com>
2016-03-16 19:12:28 +0000

    Lesson 4

--- lesson_4/tictactoe.rb
+++ lesson_4/tictactoe.rb
@@ -1,5 +1,6 @@
-require 'pry'
-
+WINNING_LINES =   [[1, 2, 3], [4, 5, 6], [7, 8, 9]] +   #rows
+                  [[2, 5, 8], [1, 4, 7], [3, 6, 9]] + #columns
+                  [[1, 5, 9], [3, 5, 7]]           #diagonals
 INITIAL_MARKER = ' '
 PLAYER_MARKER = 'X'
 COMPUTER_MARKER = 'O'
@@ -9,6 +10,7 @@ def prompt(msg)
   puts "==>#{msg}"
 end
 
+# rubocop:disable Metrics/MethodLength, Metrics/AbcSize
 def display_board(brd)
   system 'clear'
   puts ""
@@ -25,6 +27,7 @@ def display_board(brd)
   puts "     |     |"
   puts ""
 end
+# rubocop:enable Metrics/MethodLength, Metrics/AbcSize
 
 def initialize_board
   new_board = {}
@@ -43,7 +46,7 @@ def player_places_piece!(brd)
     prompt "Choose a square (#{empty_squares(brd).join})"
     square = gets.chomp.to_i
    break if empty_squares(brd).include?(square)
-      prompt "Sorry, that's not a valid choice"
+      prompt "Sorry, thats not a valid choice"
   end
   brd[square] = PLAYER_MARKER
 end
@@ -62,29 +65,30 @@ def someone_won?(brd)
 end
 
 def detect_winner(brd)
-  winning_lines = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] +   #rows
-                  [[2, 5, 8], [1, 4, 7], [3, 6, 9]] + #columns
-                  [[1, 5, 9], [3, 5, 7]]           #diagonals
-  
-  winning_lines.each do |line|
-    if brd[line[0]] == PLAYER_MARKER &&
-       brd[line[1]] == PLAYER_MARKER &&
-       brd[line[2]] == PLAYER_MARKER
+  WINNING_LINES.each do |line|
+  #   if brd[line[0]] == PLAYER_MARKER &&
+  #     brd[line[1]] == PLAYER_MARKER &&
+  #     brd[line[2]] == PLAYER_MARKER
+  #     return 'Player'
+  #   elsif brd[line[0]] == COMPUTER_MARKER &&
+  #     brd[line[1]] == COMPUTER_MARKER &&
+  #     brd[line[2]] == COMPUTER_MARKER
+  #     return 'Computer'
+  #   end
+    if brd.values_at(*line).count(PLAYER_MARKER) == 3
       return "Player"
-    elsif brd[line[0]] == COMPUTER_MARKER &&
-       brd[line[1]] == COMPUTER_MARKER &&
-       brd[line[2]] == COMPUTER_MARKER
+    elsif brd.values_at(*line).count(COMPUTER_MARKER) == 3
       return "Computer"
     end
-  end    
+    end    
  nil
 end
 
 loop do
 board = initialize_board
 
-loop do
-  display_board(board)
+  loop do
+    display_board(board)
 
   player_places_piece!(board)
   break if someone_won?(board) || board_full?(board)
@@ -98,7 +102,7 @@ display_board(board)
 if someone_won?(board)
   prompt "#{detect_winner(board)} won!"
 else
-  prompt "It's a tie!"
+  prompt "Its a tie!"
 end
 
   prompt "Play again? (y or n)"
@@ -106,4 +110,4 @@ end
 break unless answer.downcase.start_with?('y')
 end
 
-prompt "Thanks for playing tic tac toe!"
+prompt 'Thanks for playing tic tac toe!'
